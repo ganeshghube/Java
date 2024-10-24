e0200de6cbcc11fb49979e0abfec43fd8e569a60
Ganesh Ghube <ganesh.ghube@gmail.com>
2024-10-24 13:33:13 +0100

    sample code

--- src/com/ganesh/Main.java
+++ src/com/ganesh/Main.java
@@ -1,7 +1,11 @@
+package com.ganesh;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        ganesh.sayHi();
+
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
---
+++ src/com/ganesh/ganesh.java
@@ -0,0 +1,16 @@
+package com.ganesh;
+
+public class ganesh {
+    public static void main(String[] args) {
+        sayHi();
+        sayHello();
+        System.out.println("Hi from Main");
+    }
+    public static void sayHi() {
+        System.out.println("Hi from Ganesh");
+    }
+    private static void sayHello() {
+        System.out.println("Hi from Private Ganesh");
+    }
+}
+
